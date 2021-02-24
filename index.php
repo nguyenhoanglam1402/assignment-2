f549d51a7869778ff55e09fb912ca7720721062e
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 01:07:21 +0700

    Tran Thi Minh Y

--- index.php
+++ index.php
@@ -64,10 +64,10 @@
       </div>
     </div>
   </div>
-  <?php include 'connect.php';?>
-  <script src="js/jquery-3.3.1.min.js"></script>
-  <script src="js/popper.min.js"></script>
-  <script src="js/bootstrap.min.js"></script>
-  <script src="js/main.js"></script>
+    <?php include 'connect.php';?>
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
   </body>
 </html>
