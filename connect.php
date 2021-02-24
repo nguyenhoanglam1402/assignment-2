5873818fd47abc8f30b894b5787b18fff204be26
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 00:46:24 +0700

    #8

--- connect.php
+++ connect.php
@@ -1,6 +1,6 @@
 <?php
     try{
-        $connectString = "host=ec2-3-95-85-91.compute-1.amazonaws.com port=5432 dbname=dbjmg9ndaoiolh user=buydnsimuwfwbm password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"
+        $connectString = "host=ec2-3-95-85-91.compute-1.amazonaws.com port=5432 dbname=dbjmg9ndaoiolh user=buydnsimuwfwbm password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
         $databaseConnect = pg_connect($connectString);
     }
     catch(PDOException $exception){
