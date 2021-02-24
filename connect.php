8f276294bc55b3a49d6ce564b98daf94983dfcce
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-24 23:55:27 +0700

    First commit

---
+++ connect.php
@@ -0,0 +1,10 @@
+<?php
+    try{
+        $connectString = "pgsql:host=ec2-52-70-67-123.compute-1.amazonaws.com port=5432 dbname=d5barcegik3amu user=svaqldrcgpejoy password=d9fd58fbb0376a5548b50f9c195895704644ad66ce5a62d0b7ecd11be0f5ff94"
+        $databaseConnect = pg_connect($connectString);
+    }
+    catch(PODException $exception){
+        echo $exception->getMessage();
+    }
+    
+?>
---
+++ login-form-05/login-form-05/header.php
@@ -0,0 +1,8 @@
+<?php
+    if(isset($_POST["submit"])){
+        if($_POST["username"] != null && $_POST["password"]){
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+        }
+    }
+?>
