f365cdd38a3fa4ea6e2afae77d8b61bc3bf6e85f
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 02:43:40 +0700

    add alert

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     try{
         $databaseConnect = new PDO($connectString);
-        echo "Hello mình đã kết nối được database !";
+        echo '<script>alert("Hello ! I can access the database right now !");</script>';
     }
     catch(PDOException $exception){
         echo $exception->getMessage();
