503503f8cb82a134850dc25dd506c891674e525d
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 13:04:29 +0700

    #1

--- connect.php
+++ connect.php
@@ -2,10 +2,8 @@
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     try{
         $databaseConnect = pg_connect($connectString);
-        $_SESSION["databaseConnect"] = $databaseConnect;
+        $_SESSION["databaseConnect"] = $connectString;
         echo '<script>alert("Connect Successfully");</script>';
-        echo '<script>alert("'.$_SESSION["databaseConnect"].'");</script>';
-        echo '<script>alert("'.$_databaseConnect.'");</script>';
     }
     catch(PDOException $exception){
         echo $exception->getMessage();
