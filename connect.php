190a251b95d58c515697d8845ec4cfd45662ee98
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 00:17:26 +0700

    dismemetmoimayoih

--- connect.php
+++ connect.php
@@ -2,24 +2,11 @@
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     try{
         $databaseConnect = pg_connect($connectString);
-        if(isset($_POST["submit"])){
-            if($_POST["username"] && $_POST["password"]){
-                $username = $_POST['username'];
-                $password = $_POST['password'];
-                $query = "SELECT is_admin FROM users WHERE username = '$username' AND passwords = '$password'";
-                $data = pg_query($databaseConnect,$query); 
-                $login_check = pg_num_rows($data);
-                if($login_check > 0){
-                    echo '<script>alert("'.$login_check.'!")</script>';
-                }
-                else{
-                    echo '<script>alert("Login fail !")</script>';
-                }
-            }
-            
-        }else{
-            echo '<script>alert("Connect successfully !")</script>';
-        }
+        $query = "SELECT is_admin FROM users WHERE username = 'lamnhgcd191296@fpt.edu.vn' AND passwords = 'lam1234++56789'";
+        $data = pg_query($databaseConnect,$query); 
+        $login_check = pg_num_rows($data);
+        echo '<p>$login_check</p>'
+
     }
     catch(Exception $exception){
         echo '<script>alert("Connect fail !")</script>';
