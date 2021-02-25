341fc9c8ee64c651180808acb1ded9af1588e94f
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 11:58:29 +0700

    Modifying connect function

--- connect.php
+++ connect.php
@@ -1,9 +1,10 @@
 <?php
-    
+    session_start();
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     try{
-        $databaseConnect = new PDO($connectString);
-        echo '<script>alert("Hello ! I can access the database right now !");</script>';
+        $databaseConnect = pg_connect($connectString);
+        $_SESSION["databaseConnect"] = $databaseConnect;
+        echo '<script>alert("Connect Successfully");</script>';
     }
     catch(PDOException $exception){
         echo $exception->getMessage();
