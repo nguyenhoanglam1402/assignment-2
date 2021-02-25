c37f446a64ee1f5e01f13071e1fd9bcacf360f06
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-25 14:04:04 +0700

    testing 3

--- header.php
+++ header.php
@@ -1,8 +1,20 @@
 <?php
+
+    $databaseConnect = $_SESSION["databaseConnect"];
     if(isset($_POST["submit"])){
         if($_POST["username"] != null && $_POST["password"]){
             $username = $_POST["username"];
             $password = $_POST["password"];
+            $query = "SELECT is_admin FROM users WHERE username = ".pg_escape_string($username)."AND passwords= ".pg_escape_string($password)";";
+            $data = pg_query($databaseConnect,$query); 
+            $login_check = pg_num_rows($data);
+            if($login_check > 0){
+                echo '<script>console.log("Login successfully !")</script>';
+            }
+            else{
+                echo '<script>console.log("Login fail !")</script>';
+            }
         }
+        
     }
 ?>
