96785f8032cf59a9a35e83254bdd8d2e4cfee7e6
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 12:19:09 +0700

    MetCode23

--- connect.php
+++ connect.php
@@ -1,5 +1,5 @@
 <?php
-    $connectString = "host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
+    $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     $account = pg_connect($connectString);
     if ($account === false) {
         die("ERROR: Could not connect to the database server!");
