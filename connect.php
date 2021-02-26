1ff54ea4f32e0a4e6bcfefa7023d46edd06a84f3
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 12:10:36 +0700

    MetCode20

--- connect.php
+++ connect.php
@@ -2,12 +2,9 @@
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     if ($account === false) {
         die("ERROR: Could not connect to the database server!");
-      } else {
-        echo ("Connect successfully! ");
-      
+      } else {      
         $username = $_POST['username'];
         $password = $_POST['password'];
-      
         $query = "SELECT * FROM staff WHERE username = '$username' AND \"passwords\" = '$password'";
         $result = pg_query($account, $query);
         $count = pg_num_rows($result);
