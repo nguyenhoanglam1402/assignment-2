84e5141075260b533721ecf8777c1070bd019692
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 12:07:50 +0700

    MetCode18

--- connect.php
+++ connect.php
@@ -1,22 +1,24 @@
 <?php
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
-    $databaseConnect = pg_connect($connectString);
-    if($databaseConnect){
-        echo '<script>alert("Connect successfully !")</script>'
+    if ($account === false) {
+        die("ERROR: Could not connect to the database server!");
+      } else {
+        echo ("Connect successfully! ");
+      
         $username = $_POST['username'];
         $password = $_POST['password'];
-        $query = "SELECT * FROM users WHERE username = '$username' AND passwords = '$password'";
-        $data = pg_query($databaseConnect,$query);
-        $login_check = pg_num_rows($data);
-        if($login_check == 1){
-            header('Location: /management.php');
+      
+        $query = "SELECT * FROM staff WHERE username = '$username' AND \"passwords\" = '$password'";
+        $result = pg_query($account, $query);
+        $count = pg_num_rows($result);
+        if ($count == 1) {
+          session_start();
+          $_SESSION["username"] = $username;
+          header('Location: /productform.php');
+        } else {
+          echo ("Wrong username or password. Please try again!") . pg_errormessage($query);
+          header('refresh: 2; url=/index.php'); //wrong reset
         }
-        else{
-            header('Location : /index.php');
-        }
-    }
-    else{
-        echo '<script>alert("Connect fail !")</script>'
-    }
-    pg_close($account);
+      }
+      pg_close($account);
 ?>
