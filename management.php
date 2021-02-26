0624a19593b7a047b0336ccaebf0bb7de7ebe651
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 18:56:22 +0700

    hellocuocdoi10

--- management.php
+++ management.php
@@ -1,16 +1,9 @@
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-  <?php 
+<html>
+	<head>
+		<title>PHP Test</title>
+	</head>
+	<body>
+		<?php 
 			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
 			# Heroku credential 
             $host = "ec2-3-95-85-91.compute-1.amazonaws.com";
@@ -18,9 +11,8 @@
             $dbname = "dbjmg9ndaoiolh";
             $user = "buydnsimuwfwbm";
             $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			$pg_heroku = pg_connect($conn_string);
+            $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+			$pg_heroku = pg_connect($connect_string);
 			
 			if (!$pg_heroku)
 			{
@@ -61,10 +53,5 @@
 			echo '</table></body></html>';
 
 		?> 
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
+	</body>
 </html>
