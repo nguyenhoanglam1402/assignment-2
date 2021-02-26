428cfe784a60de8496dbe8955585ac1f82a69d02
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 11:32:33 +0700

    MetCode10

--- connect.php
+++ connect.php
@@ -3,7 +3,7 @@
     $databaseConnect = pg_connect($connectString);
     $username = $_POST['username'];
     $password = $_POST['password'];
-    //$query = "SELECT is_admin FROM users WHERE username = '$username' AND passwords = '$password'";
+    $query = "SELECT is_admin FROM users WHERE username = '$username' AND passwords = '$password';";
     //$data = pg_query($databaseConnect,$query); 
     //$login_check = pg_num_rows($data);
     header('Location: /management.php');
