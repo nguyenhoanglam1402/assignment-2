fadf649e4cb2b877adbf94242f6782f475c6a004
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 11:03:05 +0700

    MetCode2

--- connect.php
+++ connect.php
@@ -6,7 +6,12 @@
         $data = pg_query($databaseConnect,$query); 
         $login_check = pg_num_rows($data);
         if($login_check > 0){
-            echo'<p>$login_check</p>'
+            echo'$login_check';
+            header('Location:/management.php');
+        }
+        else{
+            echo'<script>alert("Login fail")</script>';
+            header('refresh:2, url=/index.php');
         }
     }
     catch(Exception $exception){
@@ -14,23 +19,3 @@
         echo $exception->getMessage();
     }  
 ?>
-<!doctype html>
-<html lang="en">
-  <head>
-    <title>Title</title>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-  </head>
-  <body>
-      
-    <!-- Optional JavaScript -->
-    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-  </body>
-</html>
