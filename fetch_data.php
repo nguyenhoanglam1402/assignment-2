caa1b3f8f5ce41d2201a30e50d73cefeb0fb81c1
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 17:58:39 +0700

    MetLolQua8

--- fetch_data.php
+++ fetch_data.php
@@ -12,7 +12,24 @@
     
     while($index < pg_num_rows($result)){
         $fieldName = pg_field_name($result, $index);
+        echo '<tr>';
         echo '<td>'.$fieldName.'</td>';
+        
         $index ++;
     }
+    echo '</tr>';
+    while ($row = pg_fetch_row($result)){
+		echo '<tr>';
+    	$count = count($row);
+		$y = 0;
+		while ($y < $count){
+			$c_row = current($row);
+			echo '<td>' . $c_row . '</td>';
+            next($row);
+			$y = $y + 1;
+		}
+		echo '</tr>';
+		$i = $i + 1;
+	}
+	pg_free_result($result);
 ?>
