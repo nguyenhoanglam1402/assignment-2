3b76ecda208685aa7eceea13568ae6a287b2a55d
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 11:54:39 +0700

    MetCode14

--- connect.php
+++ connect.php
@@ -1,15 +1,21 @@
 <?php
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
     $databaseConnect = pg_connect($connectString);
-    $username = $_POST['username'];
-    $password = $_POST['password'];
-    $query = "SELECT is_admin FROM users WHERE username = '$username' AND passwords = '$password';";
-    $data = pg_query($databaseConnect,$query);
-    $login_check = pg_num_rows($data);
-    if($login_check === 1){
-        header('Location: /management.php');
-    }
+    if($databaseConnect){
+        echo '<script>alert("Connect successfully !")</script>'
+        $username = $_POST['username'];
+        $password = $_POST['password'];
+        $query = "SELECT is_admin FROM users WHERE username = '$username' AND passwords = '$password';";
+        $data = pg_query($databaseConnect,$query);
+        $login_check = pg_num_rows($data);
+        if($login_check == 1){
+            header('Location: /management.php');
+        }
     else{
         header('Location : /index.php');
+    }
+    }
+    else{
+        echo '<script>alert("Connect fail !")</script>'
     }
 ?>
