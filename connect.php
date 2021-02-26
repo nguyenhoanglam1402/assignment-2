b9171d328d126aeaf5bc7fdc5d661b3e64804a02
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 11:05:01 +0700

    MetCode3

--- connect.php
+++ connect.php
@@ -1,7 +1,6 @@
 <?php
     $connectString = "pgsql:host=ec2-3-95-85-91.compute-1.amazonaws.com;port=5432;dbname=dbjmg9ndaoiolh;user=buydnsimuwfwbm;password=44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6";
-    try{
-        $databaseConnect = pg_connect($connectString);
+    $databaseConnect = pg_connect($connectString);
         $query = "SELECT is_admin FROM users WHERE username = '$_POST['username']' AND passwords = '$_POST['password']'";
         $data = pg_query($databaseConnect,$query); 
         $login_check = pg_num_rows($data);
@@ -13,9 +12,4 @@
             echo'<script>alert("Login fail")</script>';
             header('refresh:2, url=/index.php');
         }
-    }
-    catch(Exception $exception){
-        echo '<script>alert("Connect fail !")</script>';
-        echo $exception->getMessage();
-    }  
 ?>
