70dafcd88aef501d57ff5759285d7e293c0b93e0
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 18:07:12 +0700

    hellocuocdoi1

--- management.php
+++ management.php
@@ -11,15 +11,6 @@
   </head>
   <body>
   <?php 
-			echo '<p>TEST HEROKU POSTGRESQL DATABASE </p>'; 
-			# Heroku credential 
-			$host_heroku = "ec2-18-206-84-251.compute-1.amazonaws.com";
-			$db_heroku = "d8k42dnhtd0o9i";
-			$user_heroku = "crmjpgdtqgprga";
-			$pw_heroku = "0d86d0fb5f24be75ffb6728bb2ffaa6762b75489e8923fda3cdf71c519a99d67";
-			# Create connection to Heroku Postgres
-			$conn_string = "host=$host_heroku port=5432 dbname=$db_heroku user=$user_heroku password=$pw_heroku";
-			$pg_heroku = pg_connect($conn_string);
 			
 			if (!$pg_heroku)
 			{
@@ -27,7 +18,7 @@
 			}
 			# Get data by query
 			$query = 'select * from test_lab6';
-			$result = pg_query($pg_heroku, $query);
+			$result = pg_query($query);
 			# Display data column by column
 			$i = 0;
 			echo '<html><body><table><tr>';
