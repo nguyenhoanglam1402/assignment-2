4b4b52bf6eb9122d6def35ebc90a38f47e8b617d
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 16:55:08 +0700

    MetLolQua1

--- index.php
+++ index.php
@@ -1,76 +1,54 @@
-
-<!doctype html>
+<?php
+  $host = "ec2-3-95-85-91.compute-1.amazonaws.com";
+  $port = "5432";
+  $dbname = "dbjmg9ndaoiolh";
+  $user = "buydnsimuwfwbm";
+  $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
+  $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+  $dbconn = pg_connect($connection_string);
+  if(isset($_POST['submit'])&&!empty($_POST['submit'])){
+      
+      $hashpassword = $_POST['pwd'];
+      $sql ="select *from public.user where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
+      $data = pg_query($dbconn,$sql); 
+      $login_check = pg_num_rows($data);
+      if($login_check > 0){ 
+          
+          echo "Login Successfully";    
+      }else{
+          
+          echo "Invalid Details";
+      }
+  }
+?>
+<!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400&display=swap" rel="stylesheet">
-    <link rel="stylesheet" href="fonts/icomoon/style.css">
-    <link rel="stylesheet" href="css/owl.carousel.min.css">
-    <!-- Bootstrap CSS -->
-    <link rel="stylesheet" href="css/bootstrap.min.css">
-    <!-- Style -->
-    <link rel="stylesheet" href="css/style.css">
-
-    <title>Login #5</title>
-
-
-  </head>
-  <body>
-  <div class="d-md-flex half">
-    <div class="bg" style="background-image: url('images/bg_1.jpg');"></div>
-    <div class="contents">
-
-      <div class="container">
-        <div class="row align-items-center justify-content-center">
-          <div class="col-md-12">
-            <div class="form-block mx-auto">
-              <div class="text-center mb-5">
-                <h3 class="text-uppercase">Login to <strong>ATN MANAGEMENT</strong></h3>
-              </div>
-              <form action="/connect.php" method="post">
-                <div class="form-group first">
-                  <label for="username">Username</label>
-                  <input type="text" class="form-control" placeholder="your-email@gmail.com" id="username">
-                </div>
-
-                <div class="form-group last mb-3">
-                  <label for="password">Password</label>
-                  <input type="password" class="form-control" placeholder="Your Password" id="password">
-                </div>
-                
-                <div class="d-sm-flex mb-5 align-items-center">
-                  <label class="control control--checkbox mb-3 mb-sm-0"><span class="caption">Remember me</span>
-                    <input type="checkbox" checked="checked"/>
-                    <div class="control__indicator"></div>
-                  </label>
-                  <span class="ml-auto"><a href="#" class="forgot-pass">Forgot Password</a></span> 
-                </div>
-
-                <input type="submit" name="submit" id="submit" value="Log In" class="btn btn-block py-2 btn-primary">
-
-                <span class="text-center my-3 d-block">or</span>
-                
-                <div class="">
-                  
-                <a href="#" class="btn btn-block py-2 btn-facebook">
-                  <span class="icon-facebook mr-3"></span> Login with facebook
-                </a>
-
-                <a href="#" class="btn btn-block py-2 btn-google"><span class="icon-google mr-3"></span> Login with Google</a>
-                </div>
-              </form>
-            </div>
-          </div>
-        </div>
-      </div>
+<head>
+  <title>PHP PostgreSQL Registration & Login Example </title>
+  <meta name="keywords" content="PHP,PostgreSQL,Insert,Login">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body>
+<div class="container">
+  <h2>Login Here </h2>
+  <form method="post">
+  
+     
+    <div class="form-group">
+      <label for="email">Email:</label>
+      <input type="email" class="form-control" id="email" placeholder="Enter email" name="email">
     </div>
-  </div>
-    <?php include 'connect.php';?>
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/popper.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script src="js/main.js"></script>
-  </body>
+    
+     
+    <div class="form-group">
+      <label for="pwd">Password:</label>
+      <input type="password" class="form-control" id="pwd" placeholder="Enter password" name="pwd">
+    </div>
+     
+    <input type="submit" name="submit" class="btn btn-primary" value="Submit">
+  </form>
+</div>
+</body>
 </html>
