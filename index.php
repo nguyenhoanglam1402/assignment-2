916585a3ba26058fc259b480468f7c94bedbf0f5
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-26 16:58:16 +0700

    MetLolQua2

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
   if(isset($_POST['submit'])&&!empty($_POST['submit'])){
       
       $hashpassword = $_POST['pwd'];
-      $sql ="select *from public.user where email = '".pg_escape_string($_POST['email'])."' and password ='".$hashpassword."'";
+      $sql ="select *from users where username = '".pg_escape_string($_POST['email'])."' and passwords ='".$hashpassword."'";
       $data = pg_query($dbconn,$sql); 
       $login_check = pg_num_rows($data);
       if($login_check > 0){ 
