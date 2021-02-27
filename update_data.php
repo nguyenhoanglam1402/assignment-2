0524f4dfdc4505b4ea456c4fd5ae11354f27ebc4
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-28 02:04:38 +0700

    hellocuocdoi54

--- update_data.php
+++ update_data.php
@@ -5,7 +5,7 @@
         $product_inventory = $_POST['product-inventory'];
         $product_revenue = $_POST['product-revenue'];
         echo 'Ok';
-        die $query = "update category set quality_sold =".$product_sold." , revenue = ".$product_revenue.", amount = ".$product_inventory."where cid =".$product_id.;
+        die $query = "update category set quality_sold =".$product_sold." , revenue = ".$product_revenue.", amount = ".$product_inventory." where cid = ".$product_id.;
         $result = pg_query($dbconn,$query);
     }
 ?>
