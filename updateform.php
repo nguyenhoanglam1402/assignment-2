5bb94c8cb9bf44362e6f3585bd1e577f6401f840
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-28 04:26:33 +0700

    hellocuocdoi66

--- updateform.php
+++ updateform.php
@@ -1,13 +1,3 @@
-<?php
-    $host = "ec2-3-95-85-91.compute-1.amazonaws.com";
-    $port = "5432";
-    $dbname = "dbjmg9ndaoiolh";
-    $user = "buydnsimuwfwbm";
-    $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
-    $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-    $dbconn = pg_connect($connection_string) or die('Connection fail !');
-?>
-
 <!doctype html>
 <html lang="en">
   <head>
@@ -40,7 +30,7 @@
                     <label class="form-label">Revenue</label>
                     <input type="number" class="form-control" name="product-revenue" id="product-revenue">
                 </div>
-                <button type="submit" class="btn btn-primary" name="submit" id="submit">Submit</button>
+                <button type="submit" class="btn btn-primary" name="submit" id="submit" value = "Submit">Submit</button>
               </form>
           </div>
       </div>
