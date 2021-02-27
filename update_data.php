f3025aadcac4b1e9a28775a607e31843eeba7115
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-28 04:03:57 +0700

    hellocuocdoi60

--- update_data.php
+++ update_data.php
@@ -5,20 +5,22 @@
     $user = "buydnsimuwfwbm";
     $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
     $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-    $dbconn = pg_connect($connection_string);
+    $dbconn = pg_connect($connection_string) or die('Connection fail !');
     if(isset($_POST['submit'])&&!empty($_POST['submit'])){
         $product_id = $_POST['product-id'];
         $product_sold = $_POST['sold'];
         $product_inventory = $_POST['product-inventory'];
         $product_revenue = $_POST['product-revenue'];
-        $data_to_update = array('revenue' => $product_revenue, 'quality_sold' => $product_sold, 'amount' => $product_inventory);
-        $condition = array('cid' => $product_id);
-        $result = pg_update($dbconn, 'category', $data_to_update, $condition);
+        $query = "update category set(revenue, amount, product_sold) = ($product_revenue, $product_inventory, $product_sold) where cid = $product_id";
+        $result = pg_query($dbconn, $query);
         if($result){
             echo 'Updated successfully !';
+            header('Location: /updateform.php');
         }
         else{
             echo 'Updated unsuccessfully !';
+            header('Location: /updateform.php');
         }
+        pg_close($dbconn);
     }
 ?>
