03cecf44c4c1b6420e09391ee090e2d71db86162
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-28 01:55:11 +0700

    hellocuocdoi52

--- update_data.php
+++ update_data.php
@@ -1,11 +1,4 @@
 <?php
-    $host = "ec2-3-95-85-91.compute-1.amazonaws.com";
-    $port = "5432";
-    $dbname = "dbjmg9ndaoiolh";
-    $user = "buydnsimuwfwbm";
-    $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
-    $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
-    $dbconn = pg_connect($connection_string);
     if(isset($_POST['submit'])&&!empty($_POST['submit'])){
         $product_id = $_POST['product-id'];
         $product_sold = $_POST['sold'];
