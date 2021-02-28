203e50802af891df33899f4446755cf74e5776e3
nguyenhoanglam1402 <lamnhgcd191296@fpt.edu.vn>
2021-02-28 17:58:02 +0700

    hellocuocdoi94

---
+++ delete_data.php
@@ -0,0 +1,23 @@
+<?php
+      $host = "ec2-3-95-85-91.compute-1.amazonaws.com";
+      $port = "5432";
+      $dbname = "dbjmg9ndaoiolh";
+      $user = "buydnsimuwfwbm";
+      $password = "44fa8e498c06f1c49d67ea398602bcf0f140346a1486b97289102ddd8711eed6"; 
+      $connection_string = "host={$host} port={$port} dbname={$dbname} user={$user} password={$password} ";
+      $dbconn = pg_connect($connection_string);
+      if(isset($_GET['submit']) && !empty($_GET['submit'])){
+        $product_id = $_GET['product-id'];
+        $query = "DELETE FROM category WHERE cid = $product_id";
+        $result = pg_query($dbconn,$query);
+        pg_close($dbconn);
+        if($result){
+            echo '<script>alert("Deleted successfully !");</script>';
+            header('Location: /shopmanagerpage.html');
+        }
+        else{
+            echo '<script>alert("Deleted unsuccessfully !");</script>';
+            header('Location: /deletepage.html');
+        }
+      }
+?>
